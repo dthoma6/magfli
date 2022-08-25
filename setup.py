fc856c0ee43f7762c139e273bea3ab46ccb09ed1
dthoma6 <103387560+dthoma6@users.noreply.github.com>
2022-08-25 14:33:06 -0400

    Updated setup.py requirements

--- setup.py
+++ setup.py
@@ -8,22 +8,20 @@ Created on Sun May 22 13:21:59 2022
 
 from setuptools import setup, find_packages
 
-# Numba typically requires an older version of numpy. So put first.
 install_requires = [
-                        "numba",
                         "pandas",
                         "numpy",
-                        "joblib",
                         "matplotlib",
-                        "paraview"
+                        "swmfio",
+                        "vtk"
                     ]
 
 setup(
     name='magfli',
-    version='0.0.1',
+    version='0.8.0',
     author='Dean Thomas',
     author_email='dthoma6@gmu.edu',
     packages=find_packages(),
-    description='Trace magnetic field lines given a magnetic field',
+    description='Trace field lines given a field',
     install_requires=install_requires
 )
